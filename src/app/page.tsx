45b6ac8a734ee490eccab67f9937dfbefd04d007
Jorge Rodrigues <jorgerodrigues9@outlook.com>
2023-07-10 19:12:44 -0300

    Added background to service card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { IconTablerDevicesExclamation, IconTablerVectorBezier } from './components/icons'
+import computer from './assets/computer-min.svg'
 
 export default function Home() {
+ 
   return (
     <main>
       <h1>Home</h1>
@@ -13,17 +15,19 @@ export default function Home() {
             <h1 className='basis-3/6 text-5xl'>Build, deploy, and manage all your apps in a secure cloud workspace.</h1>
             <div className='basis-3/6'>
               <p className='mb-4'>
-              Spend more time on what matters. Start in seconds and scale to millions with a suite of powerful cloud tools and services for every type of app.
+                Spend more time on what matters. Start in seconds and scale to millions with a suite of powerful cloud tools and services for every type of app.
               </p>
               <a href='#' className='btn btn-primary'>Explore development workflow</a>
             </div>
         </div>
         <div className='grid grid-cols-3 gap-8'>
-          <div className='col-span-2 border rounded-3xl dark:bg-yellow-600' style={{ minHeight: '350px'}}>
+
+          {/* Web development */}
+          <div className='col-span-2 border rounded-3xl dark:bg-yellow-600 service-card' style={{ backgroundColor: '#e57b61', backgroundImage: `url(${computer.src})`, backgroundRepeat: 'no-repeat', backgroundPosition: '95% 5%', backgroundSize: 300, minHeight: '350px' }}>
             <div className='p-8 pt-4 h-full'>
              <div className='h-full flex flex-col gap-y-4 justify-between'>
               <div>
-                <IconTablerDevicesExclamation className='w-24 h-24' />
+                <IconTablerDevicesExclamation className='w-24 h-24 service-card-icon' />
               </div>   
               <div>
                 <h6 className='font-bold text-sm uppercase'>Lorem ipsum dolor</h6>
@@ -37,11 +41,13 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div className='border rounded-3xl dark:border-gray-800' style={{ minHeight: '350px'}}>
+
+          {/* Design */}
+          <div className='border rounded-3xl dark:border-gray-800 service-card' style={{ minHeight: '350px'}}>
             <div className='p-8 pt-4 h-full'>
              <div className='h-full flex flex-col gap-y-4 justify-between'>
               <div>
-                <IconTablerVectorBezier className='w-24 h-24' />
+                <IconTablerVectorBezier className='w-24 h-24 service-card-icon' />
               </div>   
               <div>
                 <h6 className='font-bold text-sm uppercase'>Lorem ipsum dolor</h6>
@@ -55,15 +61,11 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div className='border rounded-3xl dark:border-gray-800' style={{ minHeight: '300px'}}>
-            <div className='p-4'>Lorem ipsum dolor sit</div>
-          </div>
-          <div className='border rounded-3xl dark:border-gray-800' style={{ minHeight: '300px'}}>
-            <div className='p-4'>Lorem ipsum dolor sit</div>
-          </div>
-          <div className='border rounded-3xl dark:border-gray-800' style={{ minHeight: '300px'}}>
-            <div className='p-4'>Lorem ipsum dolor sit</div>
-          </div>
+
+          
+          <div className='border rounded-3xl dark:border-gray-800' style={{ minHeight: '300px'}}></div>
+          <div className='border rounded-3xl dark:border-gray-800' style={{ minHeight: '300px'}}></div>
+          <div className='border rounded-3xl dark:border-gray-800' style={{ minHeight: '300px'}}></div>
         </div>
       </section>
     </main>
