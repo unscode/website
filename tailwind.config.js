c6d72e0f87ab1e68031bf96c28ef8ba7bdec770d
Jorge Rodrigues <jorgerodrigues9@outlook.com>
2023-07-18 19:06:33 -0300

    Updated container sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,24 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    container: {
+      center: true,
+      screens: {
+        DEFAULT: '100%',
+        sm: '100%',
+        md: '100%',
+        lg: '1024px',
+        xl: '1024px',
+      },
+      padding: {
+        DEFAULT: '2rem',
+        sm: '8rem',
+        md: '16rem',
+        lg: 0,
+        xl: 0,
       },
     },
+    extend: {},
   },
   plugins: [],
 }
